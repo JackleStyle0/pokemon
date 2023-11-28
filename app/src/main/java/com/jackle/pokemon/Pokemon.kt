e8f17d1d185175aaee71e89f01a465ccac6920ce
Jckl3 <wacharat@odds.team>
2023-11-29 00:16:53 +0700

    Update view and set adapter to view

--- app/src/main/java/com/jackle/pokemon/Pokemon.kt
+++ app/src/main/java/com/jackle/pokemon/Pokemon.kt
@@ -8,15 +8,15 @@ data class Pokemon(
 )
 
 
-fun mockPokemonList() {
-    Pokemon("Bulbasaur",	"ฟุชิกิดาเนะ", "Grass", "")
-    Pokemon("Ivysaur",	"ฟุชิกิโซ", "Grass", "")
-    Pokemon("Venusaur",	"ฟุชิกิบานะ", "Grass", "")
-    Pokemon("Charmander",	"ฮิโตคาเงะ", "Fire", "")
-    Pokemon("Squirtle",	"เซนิกาเมะ", "Water", "")
-    Pokemon("Caterpie",	"คาเตอร์ปี", "Bug", "")
-    Pokemon("Weedle",	"บีเดิล", "Bug", "")
-    Pokemon("Pidgey",	"ป็อปโปะ", "Flying", "")
-    Pokemon("Rattata",	"โครัตตา", "Normal", "")
-    Pokemon("Pikachu",	"พิคาชู", "Eletric", "")
-}
+fun mockPokemonList() = listOf(
+    Pokemon("Bulbasaur", "ฟุชิกิดาเนะ", "Grass", ""),
+    Pokemon("Ivysaur", "ฟุชิกิโซ", "Grass", ""),
+    Pokemon("Venusaur", "ฟุชิกิบานะ", "Grass", ""),
+    Pokemon("Charmander", "ฮิโตคาเงะ", "Fire", ""),
+    Pokemon("Squirtle", "เซนิกาเมะ", "Water", ""),
+    Pokemon("Caterpie", "คาเตอร์ปี", "Bug", ""),
+    Pokemon("Weedle", "บีเดิล", "Bug", ""),
+    Pokemon("Pidgey", "ป็อปโปะ", "Flying", ""),
+    Pokemon("Rattata", "โครัตตา", "Normal", ""),
+    Pokemon("Pikachu", "พิคาชู", "Eletric", ""),
+)
--- app/src/main/java/com/jackle/pokemon/PokemonCollectionActivity.kt
+++ app/src/main/java/com/jackle/pokemon/PokemonCollectionActivity.kt
@@ -3,10 +3,22 @@ package com.jackle.pokemon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jackle.pokemon.databinding.ActivityPokemonCollectionBinding
+import com.jackle.pokemon.databinding.PokemonCardItemBinding
 
 class PokemonCollectionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPokemonCollectionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pokemon_collection)
+        binding = ActivityPokemonCollectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val adapter = PokemonAdapter(
+            mockPokemonList()
+        )
+
+        binding.recyclerView.adapter = adapter
     }
 }
