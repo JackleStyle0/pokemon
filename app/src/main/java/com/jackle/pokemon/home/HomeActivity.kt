30c23029c7a525600c802bfed6c066b8017fcf26
TeemaKrub <tm@odds.team>
2023-12-01 21:32:36 +0700

    [Teema] : Add application class and fix HomeActivity Binding

--- app/src/main/java/com/jackle/pokemon/home/HomeActivity.kt
+++ app/src/main/java/com/jackle/pokemon/home/HomeActivity.kt
@@ -2,15 +2,17 @@ package com.jackle.pokemon.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.jackle.pokemon.databinding.ActivityMainBinding
+import com.jackle.pokemon.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
