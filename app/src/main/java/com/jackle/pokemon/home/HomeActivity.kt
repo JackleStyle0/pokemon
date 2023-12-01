fdda9f2b42de85963f889cccbfd37cd83f32d2a5
krirati <kriratik@odds.team>
2023-12-01 21:24:41 +0700

    add binding home

--- app/src/main/java/com/jackle/pokemon/home/HomeActivity.kt
+++ app/src/main/java/com/jackle/pokemon/home/HomeActivity.kt
@@ -1,13 +1,16 @@
 package com.jackle.pokemon.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jackle.pokemon.R
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.appcompat.app.AppCompatActivity
+import com.jackle.pokemon.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
